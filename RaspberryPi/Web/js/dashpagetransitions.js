f493cca4c79e884bd7f5011f3e2d4ab3fd7d0a4a
Antonio FRANCO <franco.antonio63@gmail.com>
2013-10-24 19:10:57 +0200

    Second commit

---
+++ RaspberryPi/Web/js/dashpagetransitions.js
@@ -0,0 +1,162 @@
+var PageTransitions = (function() {
+
+	var $main = $( '#pt-main' ),
+		$pages = $main.children( 'div.pt-page' ),
+		$next = $( '#nextpage' ),
+		$prev = $( '#prevpage' ),
+		animcursor = 1,
+		pagesCount = $pages.length,
+		current = 0,
+		isAnimating = false,
+		endCurrPage = false,
+		endNextPage = false,
+		animEndEventNames = {
+			'WebkitAnimation' : 'webkitAnimationEnd',
+			'OAnimation' : 'oAnimationEnd',
+			'msAnimation' : 'MSAnimationEnd',
+			'animation' : 'animationend'
+		},
+		// animation end event name
+		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
+		// support css animations
+		support = Modernizr.cssanimations;
+	
+	function init() {
+
+		$pages.each( function() {
+			var $page = $( this );
+			$page.data( 'originalClassList', $page.attr( 'class' ) );
+		} );
+
+		$pages.eq( current ).addClass( 'pt-page-current' );
+
+//		$( '#dl-menu' ).dlmenu( {
+//			animationClasses : { in : 'dl-animate-in-2', out : 'dl-animate-out-2' },
+//			onLinkClick : function( el, ev ) {
+//				ev.preventDefault();
+//				nextPage( el.data( 'animation' ) );
+//			}
+//		} );
+
+		$next.on( 'click', function() {
+			if( isAnimating ) {
+				return false;
+			}
+			nextPage( 0 );
+		} );
+		$prev.on( 'click', function() {
+			if( isAnimating ) {
+				return false;
+			}
+			prevPage( 0 );
+		} );
+
+	}
+
+
+	function nextPage( animation ) {
+
+		if( isAnimating ) {
+			return false;
+		}
+
+		isAnimating = true;
+		
+		var $currPage = $pages.eq( current );
+
+		if( current < pagesCount - 1 ) {
+			++current;
+		}
+		else {
+			current = 0;
+		}
+
+		var $nextPage = $pages.eq( current ).addClass( 'pt-page-current' ),
+			outClass = '', inClass = '';
+
+		outClass = 'pt-page-moveToRight';
+		inClass = 'pt-page-moveFromLeft';
+
+		$currPage.addClass( outClass ).on( animEndEventName, function() {
+			$currPage.off( animEndEventName );
+			endCurrPage = true;
+			if( endNextPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		$nextPage.addClass( inClass ).on( animEndEventName, function() {
+			$nextPage.off( animEndEventName );
+			endNextPage = true;
+			if( endCurrPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		if( !support ) {
+			onEndAnimation( $currPage, $nextPage );
+		}
+	}
+
+	function prevPage( animation ) {
+
+		if( isAnimating ) {
+			return false;
+		}
+
+		isAnimating = true;
+		
+		var $currPage = $pages.eq( current );
+
+		if( current == 0 ) {
+			current = pagesCount - 1;
+		}
+		else {
+			--current;
+		}
+
+		var $nextPage = $pages.eq( current ).addClass( 'pt-page-current' ),
+			outClass = '', inClass = '';
+
+		outClass = 'pt-page-moveToLeft';
+		inClass = 'pt-page-moveFromRight';
+
+		$currPage.addClass( outClass ).on( animEndEventName, function() {
+			$currPage.off( animEndEventName );
+			endCurrPage = true;
+			if( endNextPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		$nextPage.addClass( inClass ).on( animEndEventName, function() {
+			$nextPage.off( animEndEventName );
+			endNextPage = true;
+			if( endCurrPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		if( !support ) {
+			onEndAnimation( $currPage, $nextPage );
+		}
+	}
+
+
+	function onEndAnimation( $outpage, $inpage ) {
+		endCurrPage = false;
+		endNextPage = false;
+		resetPage( $outpage, $inpage );
+		isAnimating = false;
+	}
+
+	function resetPage( $outpage, $inpage ) {
+		$outpage.attr( 'class', $outpage.data( 'originalClassList' ) );
+		$inpage.attr( 'class', $inpage.data( 'originalClassList' ) + ' pt-page-current' );
+	}
+
+	init();
+
+	return { init : init };
+
+})();
---
+++ RaspberryPi/WebServer/server.js
@@ -0,0 +1,144 @@
+/* -------------------------------------------
+  Node Web server for the FapCarDash
+  
+  ver 0.1    -  23/10/2013
+  Auth. A.Franco
+  
+  History 
+
+
+--------------------------------------------- */
+// define constants
+var isDebug = true;
+var port = 8000;
+var indexPage = 'fapcardashfe.html'; // the main page splashscreen
+var localBaseDir = __dirname + '/Web' ;
+var interProcessExchangeFile = __dirname + '/exchange.xml';
+
+
+// define globals variables
+var webServer = require('http').createServer(onRequest),
+  ioSock = require('socket.io').listen(app),
+  parser = new require('xml2json'),
+  fs = require('fs'),
+  url = require("url");
+
+var redisClient = require("redis");
+var controlIOSocket; // This is the io socket that partecipate the game
+
+// creating the server 
+webServer.listen(port);
+
+// Function that react request from web client
+function onRequest(req, res) {
+
+	// analyze the request 
+	var pathName = url.parse(req.url).pathname;
+	if(isDebug) console.log(' onRequest ::= Request url:' + pathName +'\n');
+	var query = url.parse(req.url).query;
+	if(isDebug) console.log(' onRequest ::= Request query:' + query +'\n');
+	var hash = url.parse(req.url).hash;
+	if(isDebug) console.log(' onRequest ::= Request hash:' + hash +'\n');
+  
+	var err;
+
+	// correct the null URL
+	pathName = ( pathName == '/'  ? indexPage : pathName );
+
+	// if the page have a query string process it, now ignore
+	// if(query != '') pathName = DoSomeThing(pathName);
+  
+	// if the page have a hash string process it, now ignore
+	// if(hash != '') pathName = DoSomeThing(pathName);
+
+	// Now we can read the file and send it ...
+	fs.readFile(localBaseDir + pathName, function(err, data) {
+
+		if (err) {
+    		console.log(" onRequest :: Error access the file:'+pathName "
+			console.log(err);     
+			res.writeHead(500);
+			res.end('Error loading the file '+pothName);  // terminate with error
+			}
+		else {
+			res.writeHead(200);
+			res.end(data);
+			}
+		if(isDebug) console.log(' onRequest ::= exit function \n');
+    	return;
+    	});
+	}
+// ------------------------------------
+
+// creating a new websocket to keep the content updated
+ioSock.sockets.on('connection', function(socket) {
+
+	// first of all create a socket for the interprocess exchange 
+	controlIOSocket = socket;
+	if(isDebug) console.log("ioSock connection ::= Create the I/O socket ");
+
+	// seconds hit a function for the file change polling mechanism (this is an extra)
+	if(interProcessExchangeFile != '') {
+	  	if(isDebug) console.log('ioSock connection ::= Watch the file: ' + interProcessExchangeFile);
+  		fs.exists(interProcessExchangeFile, function (exists) {
+  			if ( !exists) {
+  				if(isDebug) console.log('ioSock connection ::= The file to Watch not exists ! - ' + interProcessExchangeFile);
+  				}
+  			else {
+				fs.watch(interProcessExchangeFile, onXMLfileChange);
+  		 		}
+  			});
+  		}
+	});
+// --------------------------------
+
+// watching the xml file changing   
+function onXMLfileChange(curr, prev) {
+    // on file change we can read the new xml
+    fs.readFile(interProcessExchangeFile, function(err, data) {
+	    if (err) throw err;
+    	// parsing the new xml data and converting them into json file
+    	var json = parser.toJson(data);
+    	// adding the time of the last update
+    	json.time = new Date();
+    	// send the new data to the client
+    	if( controlIOSocket != null) {
+	 		controlIOSocket.volatile.emit('notification', json);
+			}
+		});
+	}
+// ------------------------------------------
+
+// -----  connect with the Redis DB ---------
+redisClient = redis.createClient();
+
+// and define the function that react to messages... 
+redisClient.client1.on("message", function (channel, message) {
+
+    if(isDebug) console.log("RedisClient ::= message from channel " + channel + ": " + message);
+
+	var item = new Object();
+	item.channel = channel;
+	item.message = message;
+
+    var jsonString = JSON.stringify(item);
+	if( controlIOSocket != null) {
+	 	controlIOSocket.volatile.emit('notification', jsonString);
+	 	}
+    });
+
+redisClient.on('subscribe', function (channel,count) {
+	if(isDebug) console.log("RedisClient ::= subscribed >" + channel + " -- " + count);
+	});
+
+// ==========================================	
+client1.incr("pip e peppo ");
+    client1.subscribe("gauge1");
+    client1.subscribe("gauge2");
+    client1.subscribe("gauge3");
+    client1.subscribe("gauge4");
+	
+	console.log("Connect & Subscribe to the Redis Server");
+// ============================================
+// -------------  EOF ---------------------------------
+
